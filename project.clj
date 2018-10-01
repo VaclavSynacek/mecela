c322d7671d2040fe6a0c9bf3afd54fe7c5225474
Vaclav Synacek <vaclav@synacek.com>
2018-10-01 18:31:20 +0200

    version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject mecela "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject mecela "0.1"
+  :description "Mutually Exlusive Collectively Exhaustive Log Analyzer"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [hawk "0.2.11"]
                  [jansi-clj "0.1.1"]
                  [org.clojure/tools.cli "0.4.1"]]
-;  :aot :all
+  :aot [mecela.core]
   :main mecela.core)
  
 ;  :jvm-opts ["-Xms1200m"])
